abb1cd7fc8487dc0317217fca019932b354dd66e
quaint <qicong@ilarge.cn>
2020-01-16 13:27:26 +0800

    optional test

---
+++ study-java-base/src/main/java/com/quaint/sp/javabase/java8/helper/Box.java
@@ -0,0 +1,14 @@
+package com.quaint.sp.javabase.java8.helper;
+
+import lombok.Data;
+
+/**
+ * @author quaint
+ * @date 2020-01-16 11:54
+ */
+@Data
+public class Box {
+
+    private Book book;
+
+}
