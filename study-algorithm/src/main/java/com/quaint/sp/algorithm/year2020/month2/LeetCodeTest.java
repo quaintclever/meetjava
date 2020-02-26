26ad501da244fbc4cacc925e6a38154aa112bad6
quaint <qicong@ilarge.cn>
2020-02-26 12:02:42 +0800

    leetcode 日期之差.

--- study-algorithm/src/main/java/com/quaint/sp/algorithm/year2020/month2/LeetCodeTest.java
+++ study-algorithm/src/main/java/com/quaint/sp/algorithm/year2020/month2/LeetCodeTest.java
@@ -1,5 +1,6 @@
 package com.quaint.sp.algorithm.year2020.month2;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,15 +13,37 @@ public class LeetCodeTest {
 
     public static void main(String[] args) {
 
-        int[] nums = {3,2,4};
-        int target = 6;
-        int[] ints = twoSum(nums, target);
-        System.out.println(ints[0]);
-        System.out.println(ints[1]);
+        int i = daysBetweenDates("2019-06-29", "2019-06-30");
+        System.out.println(i);
 
     }
 
-    public static int[] twoSum(int[] nums, int target) {
+
+    public static int daysBetweenDates(String date1, String date2) {
+
+//        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+//        Date d1 = null;
+//        Date d2 = null;
+//        try {
+//            d1 = df.parse(date1);
+//            d2 = df.parse(date2);
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+//        Long tempDay = d1.getTime()/24/60/60/1000-d2.getTime()/24/60/60/1000;
+//        int day = tempDay.intValue();
+//
+//        return day>0?day:day*-1;
+
+        LocalDate local1 = LocalDate.parse(date1);
+        LocalDate local2 = LocalDate.parse(date2);
+        int day = local1.until(local2).getDays();
+        return day;
+    }
+
+
+    private static int[] twoSum(int[] nums, int target) {
 
         int[] result = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
