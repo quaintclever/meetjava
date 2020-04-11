2e3e5303da048e91688d75379d74a36ba77b937d
quaint <qicong@ilarge.cn>
2020-04-11 17:57:45 +0800

    Contended 注解测试

---
+++ study-java-base/src/main/java/com/quaint/javabase/java8/ContendedTest.java
@@ -0,0 +1,48 @@
+package com.quaint.javabase.java8;
+
+import sun.misc.Contended;
+
+/**
+ * @author quaint
+ * @since 11 April 2020
+ */
+public class ContendedTest {
+
+//    @Contended
+//    private static volatile long[] arr = new long[2];
+
+    @Contended
+    volatile long long1;
+
+    @Contended
+    volatile long long2;
+
+
+    public static void main(String[] args) throws Exception{
+
+        ContendedTest ct = new ContendedTest();
+
+        long start = System.currentTimeMillis();
+        Thread t1 = new Thread(()->{
+            for (int i = 0; i < 1_0000_0000L; i++) {
+                ct.long1 = i;
+            }
+        });
+
+        Thread t2 = new Thread(()->{
+            for (int i = 0; i < 1_0000_0000L; i++) {
+                ct.long2 = i;
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        long end = System.currentTimeMillis();
+
+        System.out.println(end-start);
+
+    }
+
+}
