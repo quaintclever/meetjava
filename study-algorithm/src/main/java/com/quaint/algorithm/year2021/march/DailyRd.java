94f4cdb3905c6e8e9fef4764df4d54702152322b
quaintclever <857776215@qq.com>
2021-03-26 17:27:40 +0800

    :rocket: [42]接雨水暴力法

--- study-algorithm/src/main/java/com/quaint/algorithm/year2021/march/DailyRd.java
+++ study-algorithm/src/main/java/com/quaint/algorithm/year2021/march/DailyRd.java
@@ -15,6 +15,32 @@ public class DailyRd {
 
     }
 
+
+    public int trap1(int[] height) {
+        int n = height.length;
+        if (n < 3) {
+            return 0;
+        }
+
+        int ans = 0;
+        int leftMax = height[0];
+        for (int i = 1; i < n - 1; i++) {
+            // 遍历左面
+            for (int j = i - 1; j < i; j++) {
+                leftMax = Math.max(leftMax,height[j]);
+            }
+            // 遍历右面
+            int rightMax = height[i + 1];
+            for (int j = i + 2; j < n; j++) {
+                rightMax = Math.max(height[j], rightMax);
+            }
+            if (height[i] < leftMax && height[i] < rightMax) {
+                ans += Math.min(leftMax - height[i], rightMax - height[i]);
+            }
+        }
+        return ans;
+    }
+
     /**
      * ✔[20]有效的括号
      *
