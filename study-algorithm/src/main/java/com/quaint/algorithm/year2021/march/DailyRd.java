e4d0048c4ac46adbc7af24acaf56082a0746e18b
quaintclever <857776215@qq.com>
2021-03-26 13:13:35 +0800

    :rocket: 每日一题额外题代码提交

--- study-algorithm/src/main/java/com/quaint/algorithm/year2021/march/DailyRd.java
+++ study-algorithm/src/main/java/com/quaint/algorithm/year2021/march/DailyRd.java
@@ -1,5 +1,8 @@
 package com.quaint.algorithm.year2021.march;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @author quaint
  * @date 2021/3/25
@@ -7,7 +10,39 @@ package com.quaint.algorithm.year2021.march;
 public class DailyRd {
 
     public static void main(String[] args) {
+        DailyRd dailyRd = new DailyRd();
 
+
+    }
+
+    /**
+     * ✔[20]有效的括号
+     *
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        Deque<Integer> stack = new LinkedList<>();
+        String left = "([{";
+        String right = ")]}";
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            int li = left.indexOf(c);
+            int ri = right.indexOf(c);
+            if (li != -1) {
+                stack.push(li);
+            } else if (ri != -1) {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                int cs = stack.pop();
+                if (cs != ri) {
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
     }
 
 }
