f63fc6d2461db5e8de570a2dbd9068312eccbf5a
quaintclever <857776215@qq.com>
2021-03-27 22:43:29 +0800

    :rocket: [42]接雨水 使用单调栈

--- study-algorithm/src/main/java/com/quaint/algorithm/year2021/march/DailyRd.java
+++ study-algorithm/src/main/java/com/quaint/algorithm/year2021/march/DailyRd.java
@@ -15,29 +15,55 @@ public class DailyRd {
     }
 
 
+    /**
+     * ✔[42]接雨水
+     * 1. 使用stack
+     *
+     * @param height
+     * @return
+     */
+    public int trap3(int[] height) {
+        int ans = 0;
+        Deque<Integer> stack = new LinkedList<>();
+        int current = 0;
+        while (current < height.length) {
+            while (!stack.isEmpty() && height[stack.peek()] < height[current]) {
+                int top = stack.pop();
+                if (stack.isEmpty()) {
+                    break;
+                }
+                int dis = current - stack.peek() - 1;
+                int h = Math.min(height[stack.peek()], height[current]) - height[top];
+                ans += dis * h;
+            }
+            stack.push(current++);
+        }
+        return ans;
+    }
 
 
     /**
      * ✔[42]接雨水
      * 1. 暴力改进
+     *
      * @param height
      * @return
      */
     public int trap2(int[] height) {
         int n = height.length;
-        if(n < 3) {
+        if (n < 3) {
             return 0;
         }
         int ans = 0;
         int[] rightMax = new int[n];
-        rightMax[n-2] = height[n-1];
-        for(int i = n - 3; i >= 1 ; i--) {
-            rightMax[i] = Math.max(height[i+1], rightMax[i+1]);
+        rightMax[n - 2] = height[n - 1];
+        for (int i = n - 3; i >= 1; i--) {
+            rightMax[i] = Math.max(height[i + 1], rightMax[i + 1]);
         }
         int leftMax = height[0];
-        for(int i = 1; i < n - 1; i++) {
-            leftMax = Math.max(leftMax,height[i-1]);
-            if(height[i] < leftMax && height[i] < rightMax[i]) {
+        for (int i = 1; i < n - 1; i++) {
+            leftMax = Math.max(leftMax, height[i - 1]);
+            if (height[i] < leftMax && height[i] < rightMax[i]) {
                 ans += (Math.min(leftMax, rightMax[i]) - height[i]);
             }
         }
@@ -47,6 +73,7 @@ public class DailyRd {
     /**
      * ✔[42]接雨水
      * 1.暴力法
+     *
      * @param height
      * @return
      */
@@ -61,7 +88,7 @@ public class DailyRd {
         for (int i = 1; i < n - 1; i++) {
             // 遍历左面
             for (int j = i - 1; j < i; j++) {
-                leftMax = Math.max(leftMax,height[j]);
+                leftMax = Math.max(leftMax, height[j]);
             }
             // 遍历右面
             int rightMax = height[i + 1];
