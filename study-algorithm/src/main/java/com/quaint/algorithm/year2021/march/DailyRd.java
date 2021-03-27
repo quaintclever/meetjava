66f62fbb27b278d279225a0b2710120955a890e5
quaintclever <857776215@qq.com>
2021-03-27 10:16:20 +0800

    :rocket: [42]接雨水暴力改进

--- study-algorithm/src/main/java/com/quaint/algorithm/year2021/march/DailyRd.java
+++ study-algorithm/src/main/java/com/quaint/algorithm/year2021/march/DailyRd.java
@@ -12,10 +12,44 @@ public class DailyRd {
     public static void main(String[] args) {
         DailyRd dailyRd = new DailyRd();
 
-
     }
 
 
+
+
+    /**
+     * ✔[42]接雨水
+     * 1. 暴力改进
+     * @param height
+     * @return
+     */
+    public int trap2(int[] height) {
+        int n = height.length;
+        if(n < 3) {
+            return 0;
+        }
+        int ans = 0;
+        int[] rightMax = new int[n];
+        rightMax[n-2] = height[n-1];
+        for(int i = n - 3; i >= 1 ; i--) {
+            rightMax[i] = Math.max(height[i+1], rightMax[i+1]);
+        }
+        int leftMax = height[0];
+        for(int i = 1; i < n - 1; i++) {
+            leftMax = Math.max(leftMax,height[i-1]);
+            if(height[i] < leftMax && height[i] < rightMax[i]) {
+                ans += (Math.min(leftMax, rightMax[i]) - height[i]);
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * ✔[42]接雨水
+     * 1.暴力法
+     * @param height
+     * @return
+     */
     public int trap1(int[] height) {
         int n = height.length;
         if (n < 3) {
