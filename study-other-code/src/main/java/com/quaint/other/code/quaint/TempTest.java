0eeac92527905696a5da4e2b8dc3945e3c0f5bd3
quaintclever <857776215@qq.com>
2021-04-10 18:39:43 +0800

    :rocket: lcp 错误代码提交

--- study-other-code/src/main/java/com/quaint/other/code/quaint/TempTest.java
+++ study-other-code/src/main/java/com/quaint/other/code/quaint/TempTest.java
@@ -1,5 +1,8 @@
 package com.quaint.other.code.quaint;
 
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * @author quaint
  * @date 23 March 2020
@@ -7,7 +10,25 @@ package com.quaint.other.code.quaint;
  */
 public class TempTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        final AtomicLong threadCount = new AtomicLong(0);
+        ExecutorService executorService = new ThreadPoolExecutor(4, 4, 3L,
+                TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(), new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread(r, TempTest.class.getSimpleName() + "-" + threadCount.getAndIncrement());
+            }
+        });
+
+        Future<Boolean> submit = executorService.submit(() -> {
+            TimeUnit.SECONDS.sleep(1);
+            return true;
+        });
+
+        System.out.println("=======start");
+        System.out.println(submit.get());
+        System.out.println("=======end");
+
 
     }
 
